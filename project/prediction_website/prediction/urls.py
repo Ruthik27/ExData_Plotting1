f55aba0a0d5087e7b8e3a365800fe9f75f28126f
Ruthik27 <ruthikkale27@gmail.com>
2023-05-30 00:08:26 -0400

    delete contents

--- project/prediction_website/prediction/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'prediction'
-urlpatterns = [
-    path('', views.upload_file, name='upload'),  # Root URL pattern
-    path('result/<int:upload_id>/', views.result, name='result'),
-]
